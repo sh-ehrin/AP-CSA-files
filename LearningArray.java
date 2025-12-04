80b39b3b8b546e89af99d42b8a9f75b64317ac62
sh-ehrin <sockjinmail@gmail.com>
2025-12-04 08:24:51 -0500

    Add files via upload
    adding array lesson

---
+++ LearningArray.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ShehrinHabib
+ */
+public class LearningArray {
+    public static void main(String [] args){
+        //declare an array to store the grades of an exam
+
+
+        //declare an array to store the names of students
+
+
+        //instantiate the grades array so that it can hold 8 elements.
+
+
+        //instantiate the array of student names so that it can hold 4 elements. 
+
+
+        /*
+        Notes :
+        The general syntax to declare and instantiate an array is…
+
+        WRITE CODE HERE
+
+
+        If you happen to know all the elements, then you can declare and instantiate an array like this…
+
+        WRITE CODE HERE    //This must be done in one line.  
+
+        Feel free to copy and paste screenshots of my final drawings into this section. 
+        */
+        //declare and instantiate a String array and an int array using the second approach.
+
+
+        //assign 4 test scores to the grades array
+
+
+        //fill in the array with the student names       
+
+
+        //print the element at index 3 from the grades array
+
+
+        //Find the sum of the grades at 0 and 2 and print it.
+
+
+        //Print the length of the grades array.
+
+
+        //Get the length of the third name in students
+
+
+
+        //swap the name at index 1 with the name at index 2.
+    }
+}
