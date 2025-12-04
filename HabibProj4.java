a0de0dee0d624feee9f000cb04177dc8fb76fc2e
sh-ehrin <sockjinmail@gmail.com>
2025-12-04 08:42:19 -0500

    Add files via upload
    Adding proj4

---
+++ HabibProj4.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ShehrinHabib
+ */
+public class HabibProj4 {
+    public static void main(String [] args){
+        System.out.println(iCJava("none here"));
+        
+    }
+    
+    public static boolean iCJava(String input){
+        //find length
+        int len = input.length();
+        int counter = 0;
+        //loop around length of variable
+        for (int i = 0; i <= len-4; i++){
+            if (input.substring(i, i+4).compareTo("java") == 0){ //see if substring.equals "java"
+                counter++;
+            }
+        }
+        if (counter % 2 == 0 && counter != 0){ //true if counter is even AND java exists in the string
+            return true;
+        }
+        else return false;
+        
+    }
+    
+    public static boolean differentNeighbors(String input){
+        return true;
+    }
+}
