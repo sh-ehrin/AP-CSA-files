8efdac2b513436f761892de35e8965789c99b560
sh-ehrin <sockjinmail@gmail.com>
2025-12-04 08:42:57 -0500

    Update HabibProj4.java
    removed extra default commments

--- HabibProj4.java
+++ HabibProj4.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ShehrinHabib
- */
 public class HabibProj4 {
     public static void main(String [] args){
         System.out.println(iCJava("none here"));
@@ -35,3 +25,4 @@ public class HabibProj4 {
         return true;
     }
 }
+
