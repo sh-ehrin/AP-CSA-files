2efb6b7f31bffba64d4e3a4b6bc1369b38d5f622
sh-ehrin <sockjinmail@gmail.com>
2025-12-10 08:45:16 -0500

    Add files via upload
    array lesson edits 12/10/25

--- LearningArray.java
+++ LearningArray.java
@@ -2,7 +2,7 @@ public class LearningArray {
     //WHEN A METHOD IS NON-STATIC, CALL IT ON A NEW OBJECT, WHEN IT IS STATIC CALL IT ON THE CLASS
     public static void main(String [] args){
         //declare an array to store the grades of an exam
-        int[] grades = new int[10];
+        int[] grades = new int[4];
         //can write int grades[] = new int[10]
 
         //declare an array to store the names of students
@@ -32,7 +32,10 @@ public class LearningArray {
         String[] classes = {"AP Calc AB", "AP Computer Science A", "Government"};
 
         //assign 4 test scores to the grades array
-        grades[0] = 99;
+        grades[0] = 100;
+        grades[1] = 100;
+        grades[2] = 100;
+        grades[3] = 200;
         //grades[10] DOES NOT WORK!
 
         //fill in the array with the student names
@@ -63,8 +66,31 @@ public class LearningArray {
         System.out.println(grades); //address where is it stored in HEAP memory
         
         printArray(names);
+        
+        int [] array = new int[5];
+	array[0] = 0;
+        array[1] = 1;
+        array[2] = 2;
+        array[3] = 3;
+        array[4] = 4;
+        printArrayInt(array);
+        
+        printArrayInt(grades);
+        firstLast1(grades);
+        printArrayInt(grades);
+        System.out.println(arrSum(grades));
+        System.out.println(findAverage(grades));
+        System.out.println(findMax(grades));
     }
     
+    public static void printArrayInt(int[] list){
+        for (int i = 0; i < list.length; i++){
+            if(i != list.length-1)
+                System.out.print(list[i] + ", ");
+            else
+                System.out.println(list[i]);
+        }
+    }
     //WHEN A METHOD IS NON-STATIC, CALL IT ON A NEW OBJECT, WHEN IT IS STATIC CALL IT ON THE CLASS
     public static void printArray(String[] list){ //METHOD OVERLOADING!! CHANGING PARAMETERS BUT SAME METHOD NAME!
         for (int i = 0; i < list.length; i++){
@@ -74,4 +100,43 @@ public class LearningArray {
                 System.out.println(list[i]);
         }
     }
+    
+    public static void firstLast1(int[] arr){
+        arr[0] = -1;
+        arr[arr.length-1] = -1;
+    }
+    
+    public static int arrSum(int[] arr){
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+    
+    public static double findAverage(int[] g){
+        double avg = 0.0;
+        for (int i =0; i < g.length; i++){
+            avg += g[i];
+        }
+        return avg/g.length;
+    }
+    
+    public static int findMax(int[] g){
+        int max = g[0];
+        
+        for (int i = 0; i < g.length; i++){
+            if (g[i] > max){
+                max = g[i];
+            }
+        }
+        return max;
+    }
+    
+    public static int sequentialSearch(int[] g, int find){
+        return 0;
+    }
+
+
 }
