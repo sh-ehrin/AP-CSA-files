68f2f07b755f57aa28637ab7f43f681289792387
sh-ehrin <sockjinmail@gmail.com>
2026-01-20 22:48:36 -0500

    Add files via upload
    finished 2D array practice 2 of 2 1/20/26

--- Learning2DArrays.java
+++ Learning2DArrays.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package ap;
 
-/**
- *
- * @author ShehrinHabib
- */
 public class Learning2DArrays {
     public static void main(String[] args){
+        
         LearningArray printArray = new LearningArray();
         
         int[][] mat = new int[3][4];
         String[][] mat2 = new String[2][2];
-        int[][] grid = {{1,2,3},
-                        {8,4,9},
-                        {9,5,6},
-                        {6,9,4}};
-        int[][] table1 = {{1,2,3},
-                         {4,5,6},
-                         {7,8,9}};
+        int[][] grid = {{-1,2,3},
+                        {8,-4,9},
+                        {9,5,-6},
+                        {6,-9,4}};
+        int[][] table1 = {{1,2,3,4},
+                         {5,6,7,8},
+                         {9,10,11,12},
+                         {13,14,15,16}};
         int[][] jagged = { //formatting not on ap exam
             {10,11,12},
             {1,2},
@@ -37,35 +31,50 @@ public class Learning2DArrays {
         
         print2D(jagged);
         
-        System.out.println(Avg2D(grid));
+        System.out.println(Avg2D(table1));
         
-        double[][] table = new double[36][45];
+//        double[][] table = new double[36][45];
+//        
+//        for (int r = 0; r < table.length; r++){
+//            for ( int c = 0; c < table[r].length; c++){
+//                table[r][c] = 3.14;
+//            }
+//        }
+//        
+//        print2Double(table);
+//        
+//        for (int c = 0; c < table[1].length; c++){
+//            table[1][c] += 10.1;
+//        }
+//        print2Double(table);
+//        
+//        for (int c = 0; c < table[2].length; c++){
+//            table[2][c] *= -1;
+//        }
+//        print2Double(table);
+        System.out.println();
+        System.out.println(countNegs(grid));
         
-        for (int r = 0; r < table.length; r++){
-            for ( int c = 0; c < table[r].length; c++){
-                table[r][c] = 3.14;
-            }
-        }
-        
-        print2Double(table);
-        
-        for (int c = 0; c < table[1].length; c++){
-            table[1][c] += 10.1;
-        }
-        print2Double(table);
-        
-        for (int c = 0; c < table[2].length; c++){
-            table[2][c] *= -1;
-        }
-        print2Double(table);
-        
-        System.out.println(countNegs(table1));
-        
+        System.out.println();
         printArray.printArrayInt((majorDiag(table1)));
         
+        System.out.println();
         printArray.printArrayInt((minorDiag(table1)));
         
+        System.out.println();
         print2D(colMajorToRowMajor(grid));
+        
+        
+        System.out.println();
+        print2D(posToNeg2D(grid, 1, 1));
+        
+        System.out.println();
+        printArray.printArrayInt(rowMajorOrder(grid));
+        
+        rotateUp(grid);
+        
+        System.out.println();
+        printArray.printArrayInt(sumOfCols(table1, 2, 4));
     }
     
     public static void print2D(int[][] arr){
@@ -137,12 +146,80 @@ public class Learning2DArrays {
     
     public static int[][] colMajorToRowMajor(int[][] g){
         int[][] ans = new int[g[0].length][g.length];
-        int c = 0;
-        for (int i = 0; i < g.length; i++){
-            ans[i][c] = g[c+1][i];
-            c++;
+        
+        for (int r = 0; r < ans.length; r++){
+            for (int c = 0; c < ans[r].length; c++){
+                ans[r][c] = g[c][r];
+            }
         }
         return ans;
+    }
+    
+    public static int[][]posToNeg2D(int[][] mat, int row, int col){
+        
+        for (int r = row-1; r <= row+1; r++){
+            for (int c = col-1; c <= col+1; c++){
+                if (r >= 0 && r < mat.length && c >= 0 && c < mat[r].length){
+                    if (mat[r][c] != mat[row][col]){
+                        mat[r][c] *= -1;
+                    }
+                }
+            }
+        }
+        return mat;
+    }
+    
+    public static int[] rowMajorOrder(int[][] nums){
+        int len = nums.length*nums[0].length;
+        int[] ans = new int[len];
+        int count = 0;
+        
+        for (int r = 0; r < nums.length; r++){
+            for (int c = 0; c < nums[r].length; c++){
+                ans[count] = nums[r][c];
+                count++;
+            }
+        }
+        return ans;
+    }
+    
+    public static void rotateUp(int[][] g){
+        int[] a = new int[g[0].length];
+        
+        for (int i = 0; i < g[0].length; i++){
+            a[i] = g[0][i]; 
+        }
+        
+        for (int r = 0; r < g.length-1; r++){
+            for (int c = 0; c < g[r].length; c++){
+                g[r][c] = g[r+1][c];
+            }
+        }
+        
+        for (int c = 0; c < g[0].length; c++){
+            g[g.length-1][c] = a[c];
+        }
+        
+        for (int r = 0; r < g.length; r++){
+            System.out.println();
+            for (int c = 0; c < g[r].length; c++){
+                System.out.print(g[r][c] + ", ");
+            }
+        }
+        System.out.println();
+    }
+    
+    public static int[] sumOfCols(int[][] mat, int beginCol, int endCol){
+        //new int array of length of columns
+        int[] sums = new int[endCol-beginCol];
+        
+        //loop through each row of each column
+        for (int c = beginCol; c < endCol; c++){
+            for (int r = 0; r < mat.length; r++){
+                sums[c - beginCol] += mat[r][c];
+            }
+        }
+        return sums;
     }
 }
 
